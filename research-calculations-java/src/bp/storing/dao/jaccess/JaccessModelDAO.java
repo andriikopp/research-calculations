7fe6afe509c6e4abef596e8dffe29f6535e99802
Andrey Kopp <kopp93@gmail.com>
2018-03-03 08:06:57 +0200

    DAO: Jaccess implementation.

--- research-calculations-java/src/bp/storing/dao/jaccess/JaccessModelDAO.java
+++ research-calculations-java/src/bp/storing/dao/jaccess/JaccessModelDAO.java
@@ -1,28 +1,84 @@
 package bp.storing.dao.jaccess;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import com.healthmarketscience.jackcess.Row;
 
 import bp.storing.beans.Model;
 import bp.storing.dao.api.IModelDAO;
 
-public class JaccessModelDAO implements IModelDAO {
-
+public class JaccessModelDAO extends JaccessAbstractDAO implements IModelDAO {
 	@Override
 	public void store(Model model) {
-		// TODO Auto-generated method stub
+		connect();
 
+		Map<String, Object> row = new HashMap<String, Object>();
+
+		row.put("ModelId", model.getId());
+		row.put("ProcessId", model.getProcess());
+		row.put("ModelFileName", model.getFile());
+
+		try {
+			connection.addRowFromMap(row);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	@Override
 	public Model retrieveById(String id) {
-		// TODO Auto-generated method stub
+		connect();
+
+		for (Row row : connection) {
+			String modelId = row.getString("ModelId");
+
+			if (modelId.equals(id)) {
+				String processId = row.getString("ProcessId");
+				String fileName = row.getString("ModelFileName");
+
+				return new Model(modelId, processId, fileName);
+			}
+		}
+
+		return null;
+	}
+
+	@Override
+	public Model retrieveByName(String name) {
+		connect();
+
+		for (Row row : connection) {
+			String fileName = row.getString("ModelFileName");
+
+			if (fileName.equals(name)) {
+				String modelId = row.getString("ModelId");
+				String processId = row.getString("ProcessId");
+
+				return new Model(modelId, processId, fileName);
+			}
+		}
+
 		return null;
 	}
 
 	@Override
 	public List<Model> retrieve() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		connect();
 
+		List<Model> rows = new ArrayList<Model>();
+
+		for (Row row : connection) {
+			String modelId = row.getString("ModelId");
+			String processId = row.getString("ProcessId");
+			String fileName = row.getString("ModelFileName");
+
+			rows.add(new Model(modelId, processId, fileName));
+		}
+
+		return rows;
+	}
 }
--- research-calculations-java/src/bp/storing/dao/jaccess/JaccessProcessDAO.java
+++ research-calculations-java/src/bp/storing/dao/jaccess/JaccessProcessDAO.java
@@ -1,36 +1,84 @@
 package bp.storing.dao.jaccess;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import com.healthmarketscience.jackcess.Row;
 
 import bp.storing.beans.Process;
 import bp.storing.dao.api.IProcessDAO;
 
-public class JaccessProcessDAO implements IProcessDAO {
-
+public class JaccessProcessDAO extends JaccessAbstractDAO implements IProcessDAO {
 	@Override
 	public void store(Process process) {
-		// TODO Auto-generated method stub
+		connect();
 
+		Map<String, Object> row = new HashMap<String, Object>();
+
+		row.put("ProcessId", process.getId());
+		row.put("ProcessName", process.getName());
+		row.put("ProcessDescription", process.getDescription());
+
+		try {
+			connection.addRowFromMap(row);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	@Override
 	public Process retrieveById(String id) {
-		// TODO Auto-generated method stub
+		connect();
+
+		for (Row row : connection) {
+			String processId = row.getString("ProcessName");
+
+			if (processId.equals(id)) {
+				String name = row.getString("ProcessName");
+				String description = row.getString("ProcessDescription");
+
+				return new Process(processId, name, description);
+			}
+		}
+
 		return null;
 	}
 
 	@Override
 	public Process retrieveByName(String name) {
-		// TODO Auto-generated method stub
+		connect();
+
+		for (Row row : connection) {
+			String processName = row.getString("ProcessName");
+
+			if (processName.equals(name)) {
+				String processId = row.getString("ProcessId");
+				String description = row.getString("ProcessDescription");
+
+				return new Process(processId, name, description);
+			}
+		}
+
 		return null;
 	}
 
 	@Override
 	public List<Process> retrieve() {
-		// TODO Auto-generated method stub
-		List<Process> processes = new ArrayList<Process>();
-		return processes;
-	}
+		connect();
 
+		List<Process> rows = new ArrayList<Process>();
+
+		for (Row row : connection) {
+			String id = row.getString("ProcessId");
+			String name = row.getString("ProcessName");
+			String description = row.getString("ProcessDescription");
+
+			rows.add(new Process(id, name, description));
+		}
+
+		return rows;
+	}
 }
