82cf62d015752d867699cac782af8941f53957b6
Andrey Kopp <kopp93@gmail.com>
2018-03-04 15:01:50 +0200

    Storing: Dispose form on close.

--- research-calculations-java/src/bp/storing/ui/BPModelStoringFrame.java
+++ research-calculations-java/src/bp/storing/ui/BPModelStoringFrame.java
@@ -100,7 +100,7 @@ public class BPModelStoringFrame extends JFrame {
 		rdfContainer = new RDFStatementsContainer();
 
 		setTitle(WINDOW_TITLE);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 540, 540);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
