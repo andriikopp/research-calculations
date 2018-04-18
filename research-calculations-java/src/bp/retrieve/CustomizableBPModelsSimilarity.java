1d49b1f68d3dcec7d8ce75a6616e9ec1cf79457a
Andrey Kopp <kopp93@gmail.com>
2018-04-18 18:15:07 +0300

    Add some comparison features.

--- research-calculations-java/src/bp/retrieve/CustomizableBPModelsSimilarity.java
+++ research-calculations-java/src/bp/retrieve/CustomizableBPModelsSimilarity.java
@@ -1,5 +1,8 @@
 package bp.retrieve;
 
+import java.security.InvalidParameterException;
+import java.util.Arrays;
+
 import bp.retrieve.similarity.Similarity;
 
 /**
@@ -18,5 +21,67 @@ public class CustomizableBPModelsSimilarity extends BPModelsSimilarity {
 	 */
 	public void defineSimilarityMethod(Similarity similarity) {
 		similarityImpl = similarity;
+	}
+
+	/**
+	 * Allows to increase or decrease current similarity level.
+	 * 
+	 * @param similarityLevel
+	 */
+	public void modifySimilarityLevel(double similarityLevel) {
+		setSimilarityLevel(getSimilarityLevel() + similarityLevel);
+	}
+
+	/**
+	 * Overrides existing method with extra check for domain names.
+	 */
+	@Override
+	public void setDomainCoefficient(String domain, double coefficient) {
+		if (!Arrays.asList(UNITS_COEFF, SYSTEMS_COEFF, FLOW_OBJECTS_COEFF, INPUTS_COEFF, OUTPUTS_COEFF, KPIS_COEFF)
+				.contains(domain)) {
+			throw new InvalidParameterException();
+		}
+
+		super.setDomainCoefficient(domain, coefficient);
+	}
+
+	/**
+	 * Overrides existing method with extra check for similarity types.
+	 */
+	@Override
+	public void setSimilarityCoefficient(String similarity, double coefficient) {
+		if (!Arrays.asList(SEMANTIC_COEFF, STRUCTURE_COEFF).contains(similarity)) {
+			throw new InvalidParameterException();
+		}
+
+		super.setSimilarityCoefficient(similarity, coefficient);
+	}
+
+	/**
+	 * Allows to compare models only using label similarity.
+	 */
+	public void compareByLabels() {
+		setSimilarityCoefficient(SEMANTIC_COEFF, 1);
+		setSimilarityCoefficient(STRUCTURE_COEFF, 0);
+	}
+
+	/**
+	 * Allows to compare models only using structure similarity.
+	 */
+	public void compareByStructure() {
+		setSimilarityCoefficient(SEMANTIC_COEFF, 0);
+		setSimilarityCoefficient(STRUCTURE_COEFF, 1);
+	}
+
+	/**
+	 * Allows to compare models only by flow objects.
+	 */
+	public void compareByFlowObjects() {
+		setDomainCoefficient(UNITS_COEFF, 0);
+		setDomainCoefficient(SYSTEMS_COEFF, 0);
+		setDomainCoefficient(FLOW_OBJECTS_COEFF, 1);
+		setDomainCoefficient(INPUTS_COEFF, 0);
+		setDomainCoefficient(OUTPUTS_COEFF, 0);
+		setDomainCoefficient(KPIS_COEFF, 0);
 	}
 }
