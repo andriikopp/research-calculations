8a9c0332e715d74c97c5dadc7c44de6eb0f933ce
Andrii Kopp <kopp93@gmail.com>
2018-08-16 20:09:54 +0300

    Add process interface class.

---
+++ research-calculations-java/src/edu/kopp/phd/express/metamodel/entity/ProcessInterface.java
@@ -0,0 +1,8 @@
+package edu.kopp.phd.express.metamodel.entity;
+
+public class ProcessInterface extends Node {
+
+    public ProcessInterface(String label, int preceding, int subsequent) {
+        super(label, preceding, subsequent);
+    }
+}
