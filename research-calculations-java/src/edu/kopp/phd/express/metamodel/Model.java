7792c855921f6e4bef2415637d504052509a3019
Andrii Kopp <kopp93@gmail.com>
2018-09-04 17:12:19 +0300

    Density, connectivity, and balance coefficient.

--- research-calculations-java/src/edu/kopp/phd/express/metamodel/Model.java
+++ research-calculations-java/src/edu/kopp/phd/express/metamodel/Model.java
@@ -1,5 +1,6 @@
 package edu.kopp.phd.express.metamodel;
 
+import edu.kopp.phd.express.metamodel.entity.Function;
 import edu.kopp.phd.express.metamodel.entity.Node;
 
 import java.util.ArrayList;
@@ -10,6 +11,8 @@ public class Model {
 
     private List<Node> nodes;
 
+    private boolean isEnvironmentEnabled;
+
     public Model(String name) {
         this.name = name;
         this.nodes = new ArrayList<>();
@@ -17,6 +20,75 @@ public class Model {
 
     public String nextNodeLabel() {
         return "Node-" + String.valueOf(nodes.size() + 1);
+    }
+
+    public void enableEnvironment() {
+        isEnvironmentEnabled = true;
+    }
+
+    public void ignoreEnvironment() {
+        isEnvironmentEnabled = false;
+    }
+
+    public double density() {
+        double size = nodes.size();
+        double arcs = countArcs();
+
+        return arcs / (size * (size - 1));
+    }
+
+    public double connectivity() {
+        double size = nodes.size();
+        double arcs = countArcs();
+
+        return arcs / size;
+    }
+
+    public double balance() {
+        double size = nodes.size();
+
+        double sum = 0;
+        double max = 0;
+
+        for (Node node : nodes) {
+            if (node instanceof Function) {
+                Function function = (Function) node;
+
+                double arcs = function.getPreceding() + function.getSubsequent();
+
+                if (isEnvironmentEnabled) {
+                    arcs = function.getOrganizationalUnits() + function.getInputs() +
+                            function.getRegulations() + function.getOutputs() +
+                            function.getApplicationSystems();
+                }
+
+                if (arcs > max) {
+                    max = arcs;
+                }
+
+                sum += arcs;
+            }
+        }
+
+        return Math.abs(sum / size - max);
+    }
+
+    private double countArcs() {
+        double arcs = 0;
+
+        for (Node node : nodes) {
+            arcs += node.getPreceding() + node.getSubsequent();
+
+            if (isEnvironmentEnabled && node instanceof Function) {
+                Function function = (Function) node;
+
+                arcs += function.getOrganizationalUnits() + function.getInputs() +
+                        function.getRegulations() + function.getOutputs() +
+                        function.getApplicationSystems();
+            }
+        }
+
+        return arcs;
     }
 
     public String getName() {
