16f775495f655aaf9d6b0cef5fa1afc65134a6b7
Andrii Kopp <kopp93@gmail.com>
2018-10-07 17:45:48 +0300

    Compare business process models based on structure similarity.

--- research-calculations-java/src/edu/kopp/phd/express/ExpressApplication.java
+++ research-calculations-java/src/edu/kopp/phd/express/ExpressApplication.java
@@ -3,8 +3,53 @@ package edu.kopp.phd.express;
 import edu.kopp.phd.express.landscape.ARISLandscape;
 import edu.kopp.phd.express.landscape.BPMNLandscape;
 import edu.kopp.phd.express.landscape.DFDLandscape;
+import edu.kopp.phd.express.metamodel.Model;
+import edu.kopp.phd.express.search.ModelSimilarity;
+
+import java.util.*;
 
 public class ExpressApplication {
+
+    public static void compareModels() {
+        Map<Model, String> allModels = new LinkedHashMap<>();
+
+        for (Model model : new ARISLandscape().getGovernanceLog().getLandscape()) {
+            allModels.put(model, "ARISeEPC");
+        }
+
+        for (Model model : new BPMNLandscape().getGovernanceLog().getLandscape()) {
+            allModels.put(model, "BPMN");
+        }
+
+        for (Model model : new DFDLandscape().getGovernanceLog().getLandscape()) {
+            allModels.put(model, "DFD");
+        }
+
+        for (Map.Entry<Model, String> first : allModels.entrySet()) {
+            System.out.printf("%s\t", first.getKey().getName());
+
+            for (Map.Entry<Model, String> second : allModels.entrySet()) {
+                System.out.printf("%.2f\t", ModelSimilarity.similarity(first.getKey(), second.getKey(),
+                        ModelSimilarity.getCompareWeights(first.getValue(), second.getValue())));
+            }
+
+            System.out.println();
+        }
+
+        System.out.println("Quality");
+
+        for (Map.Entry<Model, String> first : allModels.entrySet()) {
+            for (Map.Entry<Model, String> second : allModels.entrySet()) {
+                System.out.printf("%s\t%s\t%.2f\t%.2f\t%.2f\n",
+                        first.getKey().getName(),
+                        second.getKey().getName(),
+                        ModelSimilarity.similarity(first.getKey(), second.getKey(),
+                                ModelSimilarity.getCompareWeights(first.getValue(), second.getValue())),
+                        first.getKey().connectivity(),
+                        second.getKey().connectivity());
+            }
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println("ARIS");
@@ -18,5 +63,8 @@ public class ExpressApplication {
 
         System.out.println("DFD");
         new DFDLandscape().getGovernanceLog().analyze();
+
+        System.out.println("Pairwise comparison");
+        compareModels();
     }
 }
