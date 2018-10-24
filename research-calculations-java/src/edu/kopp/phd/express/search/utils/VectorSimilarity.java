1256f4cce3da0fa2465e7a6411438a99db60328b
Andrii Kopp <kopp93@gmail.com>
2018-10-24 15:12:39 +0300

    VectorSimilarity - Support arrays.

--- research-calculations-java/src/edu/kopp/phd/express/search/utils/VectorSimilarity.java
+++ research-calculations-java/src/edu/kopp/phd/express/search/utils/VectorSimilarity.java
@@ -5,4 +5,14 @@ public class VectorSimilarity {
     public static double similarity(double first, double second) {
         return 1.0 / (1.0 + Math.abs(first - second));
     }
+
+    public static double similarity(double[] first, double[] second) {
+        double sum = 0;
+
+        for (int i = 0; i < first.length; i++) {
+            sum += Math.abs(first[i] - second[i]);
+        }
+
+        return 1.0 / (1.0 + sum);
+    }
 }
