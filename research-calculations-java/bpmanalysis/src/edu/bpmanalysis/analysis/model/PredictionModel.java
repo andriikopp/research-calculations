78dde5ef0e807a584cdb3736eb1c75a82109c56e
Andrii Kopp <kopp93@gmail.com>
2019-02-21 16:50:04 +0200

    Update time prediction model for business process models.

--- research-calculations-java/bpmanalysis/src/edu/bpmanalysis/analysis/model/PredictionModel.java
+++ research-calculations-java/bpmanalysis/src/edu/bpmanalysis/analysis/model/PredictionModel.java
@@ -1,31 +1,43 @@
 package edu.bpmanalysis.analysis.model;
 
-import edu.bpmanalysis.analysis.ConnectorsMismatch;
 import edu.bpmanalysis.analysis.ModelDensity;
+import edu.bpmanalysis.analysis.NodesSubsetsUtil;
 import edu.bpmanalysis.description.tools.Model;
+import edu.bpmanalysis.description.tools.Node;
 
 public class PredictionModel {
 
-    public static double understandabilityTime(Model model) {
-        return understandabilityTime(ModelDensity.size(model));
+    public static double time(Model model) {
+        return time(getTNSF(model), getTNE(model), getNSFE(model), getTNG(model));
     }
 
-    public static double modifiabilityTime(Model model) {
-        return modifiabilityTime(ConnectorsMismatch.mismatch(model), ModelDensity.density(model));
+    public static double time(double tnsf, double tne, double nsfe, double tng) {
+        return 40.622343
+                - 0.205153 * tnsf
+                + 9.562413 * tne
+                - 8.387718 * nsfe
+                + 5.411965 * tng;
     }
 
-    public static double understandabilityTime(double size) {
-        double b0 = 47.04;
-        double b1 = 2.46;
-
-        return b0 + b1 * size;
+    public static double getTNSF(Model model) {
+        return ModelDensity.arcs(model);
     }
 
-    public static double modifiabilityTime(double mismatch, double density) {
-        double b0 = 50.08;
-        double b1 = 3.77;
-        double b2 = 422.95;
+    public static double getTNE(Model model) {
+        return NodesSubsetsUtil.getEvents(model).size();
+    }
 
-        return b0 + b1 * mismatch + b2 * density;
+    public static double getNSFE(Model model) {
+        double value = 0;
+
+        for (Node node : NodesSubsetsUtil.getEvents(model)) {
+            value += node.getOutput();
+        }
+
+        return value;
+    }
+
+    public static double getTNG(Model model) {
+        return NodesSubsetsUtil.getConnectors(model).size();
     }
 }
