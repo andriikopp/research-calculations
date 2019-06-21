76c5c68d2903e50bfe92f97f1715f3f76887efa8
Andrii Kopp <kopp93@gmail.com>
2019-06-21 11:53:44 +0300

    Load models (and clean repository before) on initialization.

--- research-calculations-java/bpmanalysis/src/edu/bpmanalysis/config/Configuration.java
+++ research-calculations-java/bpmanalysis/src/edu/bpmanalysis/config/Configuration.java
@@ -6,10 +6,12 @@ import edu.bpmanalysis.search.similarity.api.Index;
 public class Configuration {
     public static final boolean DEBUG = false;
 
-    public static final String PATH_TO_BPMN_MODELS = "processModelsStorage/bpmn/";
+    public static final String PATH_TO_BPMN_MODELS = "processModelsStorage/edental/";
 
     public static final Index SIMILARITY_INDEX = new JaccardIndex();
 
     public static final double SIMILARITY_THRESHOLD = 1.0;
+
+    public static final boolean LOAD_MODELS_BEFORE_START = true;
 
 }
--- research-calculations-java/bpmanalysis/src/edu/bpmanalysis/web/WebApp.java
+++ research-calculations-java/bpmanalysis/src/edu/bpmanalysis/web/WebApp.java
@@ -1,14 +1,27 @@
 package edu.bpmanalysis.web;
 
+import edu.bpmanalysis.config.Configuration;
+import edu.bpmanalysis.description.ProcessModelImportUtil;
 import edu.bpmanalysis.web.controller.ProcessModelAnalysisControllerImpl;
 import edu.bpmanalysis.web.controller.api.ProcessModelAnalysisController;
 import edu.bpmanalysis.web.model.ProcessModelRepositoryJsonDB;
+import edu.bpmanalysis.web.model.api.ProcessModelRepository;
 
 public class WebApp {
 
     public static void main(String[] args) {
+        if (Configuration.LOAD_MODELS_BEFORE_START) {
+            ProcessModelImportUtil.cleanRepository();
+        }
+
+        ProcessModelRepository repository = new ProcessModelRepositoryJsonDB();
+
+        if (Configuration.LOAD_MODELS_BEFORE_START) {
+            ProcessModelImportUtil.importModelsFromBPMNDocuments(repository);
+        }
+
         ProcessModelAnalysisController controller = new ProcessModelAnalysisControllerImpl();
-        controller.setRepository(new ProcessModelRepositoryJsonDB());
+        controller.setRepository(repository);
 
         controller.init();
     }
