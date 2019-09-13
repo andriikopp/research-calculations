0a8b51b2492a7eef488cf7504625fbaae8251500
Andrii Kopp <kopp93@gmail.com>
2019-09-13 14:43:26 +0300

    Run BPMAI from bat file. Use text .conf file with path to models. Update .gitignore.

--- research-calculations-java/bpmanalysis/src/edu/bpmanalysis/config/Configuration.java
+++ research-calculations-java/bpmanalysis/src/edu/bpmanalysis/config/Configuration.java
@@ -2,11 +2,24 @@ package edu.bpmanalysis.config;
 
 import edu.bpmanalysis.search.similarity.JaccardIndex;
 import edu.bpmanalysis.search.similarity.api.Index;
+import org.apache.commons.io.IOUtils;
+
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Configuration {
     public static final boolean DEBUG = false;
 
-    public static final String PATH_TO_BPMN_MODELS = "processModelsStorage/test_models/";
+    public static String PATH_TO_BPMN_MODELS = null;
+
+    static {
+        try {
+            PATH_TO_BPMN_MODELS = IOUtils
+                    .toString(new FileReader("process-models-path.conf"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static final Index SIMILARITY_INDEX = new JaccardIndex();
 
