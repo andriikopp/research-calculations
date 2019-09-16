ef790d506616ff5205c412fda18163a5fed20a56
Andrii Kopp <kopp93@gmail.com>
2019-09-16 10:39:17 +0300

    Quality criterias fixed according to notations features. Duplicated code removed.

--- research-calculations-java/bpmanalysis/src/edu/bpmanalysis/analysis/model/EvaluationUtil.java
+++ research-calculations-java/bpmanalysis/src/edu/bpmanalysis/analysis/model/EvaluationUtil.java
@@ -10,46 +10,7 @@ import edu.bpmanalysis.description.tools.Model;
 public class EvaluationUtil {
 
     public static double quality(Model model) {
-        double nodes = ModelDensity.size(model);
-        double functions = NodesSubsetsUtil.getFunctions(model).size();
-
-        double guideline1 = Math.min(
-                1 - Math.signum(
-                        (model.getModelType().equals(Model.ModelType.EPC) ||
-                                model.getModelType().equals(Model.ModelType.BPMN)) ?
-                                Math.max(nodes, 31) - 31 :
-
-                                model.getModelType().equals(Model.ModelType.DFD) ?
-                                        Math.max(functions, 7) - 7 :
-
-                                        Math.max(functions, 6) - 6
-                ),
-                1 - Math.signum(
-                        (model.getModelType().equals(Model.ModelType.EPC) ||
-                                model.getModelType().equals(Model.ModelType.BPMN)) ?
-                                1 - Math.min(functions, 1) :
-
-                                model.getModelType().equals(Model.ModelType.DFD) ?
-                                        1 - Math.min(functions, 1) :
-
-                                        3 - Math.min(functions, 3)
-                )
-        );
-
-        double guideline2 = Math.min(
-                1 - Math.signum(new ConnectorsBalance().balanceCoefficient(model)),
-                1 - Math.signum(new FunctionsBalance().balanceCoefficient(model))
-        );
-
-        double guideline3 = Math.min(
-                1 - Math.signum(Math.abs(1 - NodesSubsetsUtil.getStartEvents(model).size())),
-                1 - Math.signum(Math.abs(1 - NodesSubsetsUtil.getEndEvents(model).size()))
-        );
-
-        double guideline4 = 1 - Math.signum(ConnectorsMismatch.mismatch(model));
-        double guideline5 = 1 - Math.signum(NodesSubsetsUtil.getORRoutingElements(model).size());
-
-        double guidelines[] = {guideline1, guideline2, guideline3, guideline4, guideline5};
+        double guidelines[] = checkGuidelines(model);
         double weights[];
 
         if (model.getModelType().equals(Model.ModelType.EPC) ||
@@ -100,13 +61,21 @@ public class EvaluationUtil {
                 1 - Math.signum(new FunctionsBalance().balanceCoefficient(model))
         );
 
-        double guideline3 = Math.min(
-                1 - Math.signum(Math.abs(1 - NodesSubsetsUtil.getStartEvents(model).size())),
-                1 - Math.signum(Math.abs(1 - NodesSubsetsUtil.getEndEvents(model).size()))
-        );
+        double guideline3 = (model.getModelType().equals(Model.ModelType.EPC) ||
+                model.getModelType().equals(Model.ModelType.BPMN))
+                ? (Math.min(1 - Math.signum(Math.abs(1 - NodesSubsetsUtil.getStartEvents(model).size())),
+                1 - Math.signum(Math.abs(1 - NodesSubsetsUtil.getEndEvents(model).size()))))
+                : 1;
 
-        double guideline4 = 1 - Math.signum(ConnectorsMismatch.mismatch(model));
-        double guideline5 = 1 - Math.signum(NodesSubsetsUtil.getORRoutingElements(model).size());
+        double guideline4 = (model.getModelType().equals(Model.ModelType.EPC) ||
+                model.getModelType().equals(Model.ModelType.BPMN))
+                ? (1 - Math.signum(ConnectorsMismatch.mismatch(model)))
+                : 1;
+
+        double guideline5 = (model.getModelType().equals(Model.ModelType.EPC) ||
+                model.getModelType().equals(Model.ModelType.BPMN))
+                ? (1 - Math.signum(NodesSubsetsUtil.getORRoutingElements(model).size()))
+                : 1;
 
         double guidelines[] = {guideline1, guideline2, guideline3, guideline4, guideline5};
 
