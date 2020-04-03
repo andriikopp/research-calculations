15a0b5ec4be9a744f816372f1fe2abef03de4732
Andrii Kopp <kopp93@gmail.com>
2020-04-03 18:24:37 +0300

    predictive models based on basic and calculated metrics

--- research-calculations-r/BPMN_Quality_Prediction.r
+++ research-calculations-r/BPMN_Quality_Prediction.r
@@ -10,8 +10,8 @@ library(rpart.plot)
 training <- read_excel("BPMN_Quality_DataSet.xlsx")
 training$status <- factor(training$status)
 
-# Logit
-model <- glm(nstatus ~ totalNodes + startEvents + endEvents + totalGateways, 
+# Logit (basic metrics)
+model <- glm(nstatus ~ totalNodes + events + totalGateways, 
              data = training)
 
 print(summary(model))
@@ -20,8 +20,31 @@ prediction <- predict(model, newdata = training, type = "response")
 
 confusionMatrix(as.factor(prediction < 0.5), as.factor(training$nstatus < 1))
 
-# DTree
-tree <- rpart(status ~ totalNodes + startEvents + endEvents + totalGateways, 
+# DTree (basic metrics)
+tree <- rpart(status ~ totalNodes + events + totalGateways, 
+              data = training, method = "class")
+
+rpart.plot(tree)
+print(tree)
+
+pred <- predict(tree, type="class")
+
+confusionMatrix(as.factor(pred), as.factor(training$status))
+
+# Logit (calculated metrics)
+model <- glm(nstatus ~ invalidNodes + startEvents + endEvents + unmatchedGateways + 
+               orGateways, 
+             data = training)
+
+print(summary(model))
+
+prediction <- predict(model, newdata = training, type = "response")
+
+confusionMatrix(as.factor(prediction < 0.5), as.factor(training$nstatus < 1))
+
+# DTree (calculated metrics)
+tree <- rpart(status ~ invalidNodes + startEvents + endEvents + unmatchedGateways + 
+                orGateways, 
               data = training, method = "class")
 
 rpart.plot(tree)
